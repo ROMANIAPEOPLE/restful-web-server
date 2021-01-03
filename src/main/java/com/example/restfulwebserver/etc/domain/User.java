c402093b41b93c79487debd18b903e72931baa36
jungkh405@naver.com <jungkh405@naver.com>
2021-01-03 22:01:15 +0900

    Swagger 추가

--- src/main/java/com/example/restfulwebserver/etc/domain/User.java
+++ src/main/java/com/example/restfulwebserver/etc/domain/User.java
@@ -6,6 +6,10 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
@@ -17,9 +21,12 @@ import java.util.Date;
 @NoArgsConstructor
 //@JsonFilter("UserInfo")
 @ApiModel(description = "회원 상세 정보를 위한 도메인 객체")
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
-    @Size(min=2, message = "2글자 이상 입력해주세요.")
+    @Size(min=2, message = "2글자 이상 입력해주세요..")
     @ApiModelProperty(notes ="사용자 이름을 입력해주세요.")
     private String name;
     @Past
